3fb13d17fa7bddb0d9e5b3eaed2700b44e6412f7
Jonathan Roquelaure <jonathan.roquelaure@gmail.com>
2021-02-17 15:30:28 +0100

    1.1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Enjoy SA Launch - 1.0'));
+app.get('/', (req, res) => res.send('Enjoy SA Launch - 1.1'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
